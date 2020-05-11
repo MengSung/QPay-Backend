8ab15eff8bb98dcd829019676fa3ecc6ea85d81b
eiahb3838ya <eiahb3838ya@gmail.com>
2020-05-11 14:52:51 +0800

    ---

--- QPayBackend/Tools/QPayWebhook.cs
+++ QPayBackend/Tools/QPayWebhook.cs
@@ -68,9 +68,11 @@ namespace QPayBackend.Tools
                 this.m_LineMessagingClient = new LineMessagingClient(ConvertShopNoToChannelAccessToken(aBackendPostData.ShopNo));
                 m_PushUtility = new PushUtility(m_LineMessagingClient);
 
-                this.m_PushUtility.SendMessage("U7638e4ed509708a3573ba6d69970583d", "呼叫ATM轉帳");
+                this.m_PushUtility.SendMessage(MENGSUNG_LINE_ID, "呼叫ATM轉帳");
 
                 m_ToolUtilityClass = new ToolUtilityClass("DYNAMICS365", ConvertShopNoToOrganization(aBackendPostData.ShopNo));
+
+                //this.m_PushUtility.SendMessage(MENGSUNG_LINE_ID, "呼叫ATM轉帳-建立m_ToolUtilityClass-完成");
 
                 QryOrderPay aQryOrderPay = new QryOrderPay();
 
@@ -81,6 +83,8 @@ namespace QPayBackend.Tools
                 Entity aFeeEntity;
                 if (FeeEntityCollection.Entities.Count == 1)
                 {
+                    //this.m_PushUtility.SendMessage(MENGSUNG_LINE_ID, "呼叫ATM轉帳-有找到收費單");
+
                     aFeeEntity = this.m_ToolUtilityClass.RetrieveEntity("new_fee", FeeEntityCollection.Entities[0].Id);
                     // 收費單付款狀態
                     if (this.m_ToolUtilityClass.GetOptionSetAttribute(ref aFeeEntity, "new_pay_status") == 100000001) // 100000001 = 信用卡已繳費
@@ -93,6 +97,8 @@ namespace QPayBackend.Tools
                 }
                 else
                 {
+                    //this.m_PushUtility.SendMessage( MENGSUNG_LINE_ID, "呼叫ATM轉帳-沒找到收費單");
+
                     return Json(new Dictionary<string, string>()
                     {
                         { "Status", "S" }
@@ -207,7 +213,8 @@ namespace QPayBackend.Tools
             {
                 case "NA0149_001":
                     //return @"HeuLkSEF5CX7hdZo4956IPpgJNdb8VqRZeL1Gu37kFFm+1F7DObAGjfeVYaggzwjZ5H4qraesvquODt7Y81jbtspNZkEq5n3oLDG+G32xQsRx1jCobkABL/Z7RKjkSACNT6h72bPQXsVn9aCuI5OogdB04t89/1O/w1cDnyilFU=";
-                    return @"g1jtWWNkjbH3OCh1cKoRvPBUkCJIygNuvV/neHXR9I4J5GBgVE85inaIaTcT4AAZ1qCuqrqJXDawrUweyBqLcX97GGokXnTRQ6MxjXAutd5Yr2FkPsZnq6kMelc/C+mqNUHaVUKFAuvTD8JvXbNmpAdB04t89/1O/w1cDnyilFU=";
+                    //return @"g1jtWWNkjbH3OCh1cKoRvPBUkCJIygNuvV/neHXR9I4J5GBgVE85inaIaTcT4AAZ1qCuqrqJXDawrUweyBqLcX97GGokXnTRQ6MxjXAutd5Yr2FkPsZnq6kMelc/C+mqNUHaVUKFAuvTD8JvXbNmpAdB04t89/1O/w1cDnyilFU=";
+                    return @"aKS4zYeq2ZpqlLd4gslkWAyYuiC+B2f1noatF1VylPvkR2+mrvJ7mwnIIXtn2Pi117NBmNTmRZL5DO5ZMYaGCj/v9+fB6Zn9sel42Jr55PlegJdrtoSvPgm4fBso1tY/7H65+cOFDQxjqhdOU69qQAdB04t89/1O/w1cDnyilFU=";
                 default:
                     return null;
             }
